a09ce5cce9010a5dc767e864bb04b3f753b09a77
Blaize Ottizy <Blaize Ottizy>
2017-04-20 15:23:55 +0300

    improve the function to check if both lists are similar

--- missing_number.py
+++ missing_number.py
@@ -1,14 +1,18 @@
 def find_missing(list_one, list_two):
-    """find_missing function
-
-    find the missing number between two lists
-    """
     # check if both lists are empty
     if len(list_one) == len(list_two) == 0:
         return 0
-
-    # check if both lists are similar
+    
+	# check if both lists are similar
     if not set(list_one).symmetric_difference(set(list_two)):
         return 0
+    elif list_one != list_two:
+        x = list(set(list_one) - set(list_two))
+        y = list(set(list_two) - set(list_one))
+
+        if x != []:
+            return x[0]
+        else:
+            return y[0]
     else:
-        return list(set(list_one).symmetric_difference(set(list_two)))[0]
+        return list(set(list_one).symmetric_difference(set(list_two)))
